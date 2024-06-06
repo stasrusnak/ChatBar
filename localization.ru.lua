64936ab32c678c0a43f0a3d5f200fc241082d0ef
stasrusnak <stasrusnak@gmail.com>
2024-06-06 19:11:57 +0300

    add translate

---
+++ localization.ru.lua
@@ -0,0 +1,58 @@
+--------------------------------------------------
+-- localization.lua (English)
+--------------------------------------------------
+
+CHATBAR_SAY_ABRV			= "S";
+CHATBAR_YELL_ABRV			= "Y";
+CHATBAR_PARTY_ABRV			= "P";
+CHATBAR_RAID_ABRV			= "R";
+CHATBAR_RAID_WARNING_ABRV	= "A";
+CHATBAR_BATTLEGROUND_ABRV	= "B";
+CHATBAR_GUILD_ABRV			= "G";
+CHATBAR_OFFICER_ABRV		= "O";
+CHATBAR_WHISPER_ABRV		= "W";
+CHATBAR_EMOTE_ABRV			= "E";
+
+CHATBAR_MENU_MAIN_TITLE 			= "Настройки";
+CHATBAR_MENU_MAIN_VERTICAL 			= "Верт. ориентация";
+CHATBAR_MENU_MAIN_REVERSE 			= "В обратном порядке";
+CHATBAR_MENU_MAIN_ALTART 			= "Вид кубики";
+CHATBAR_MENU_MAIN_TEXTONBUTTONS	 	= "Текст на кнопках";
+CHATBAR_MENU_MAIN_SHOWTEXT 			= "Вкл. текст";
+CHATBAR_MENU_MAIN_CHANNELID 		= "ID канала на кнопке";
+CHATBAR_MENU_MAIN_BUTTONFLASHING	= "Button Message Flashing";
+CHATBAR_MENU_MAIN_BARBORDER 		= "Показать границы";
+CHATBAR_MENU_MAIN_ADDONCHANNELS 	= "Hide Addon Channels";
+CHATBAR_MENU_MAIN_HIDEALL			= "Скрыть все кнопки";
+CHATBAR_MENU_MAIN_HIDDENBUTTONS		= "Hidden Buttons";
+CHATBAR_MENU_MAIN_RESET				= "Сбросить позицию";
+CHATBAR_MENU_MAIN_REORDER			= "Reorder Channels";
+
+CHATBAR_MENU_CHANNEL_LEAVE			= "Покинуть";
+CHATBAR_MENU_CHANNEL_LIST			= "список User List";
+CHATBAR_MENU_HIDE_BUTTON			= "Hide \"%s\" Button";
+CHATBAR_MENU_WHISPER_REPLY			= "Reply";
+CHATBAR_MENU_WHISPER_RETELL			= "Retell";
+CHATBAR_MENU_CHANNEL_STICKY			= "Sticky Channels";
+CHATBAR_MENU_STICKY					= "Sticky";
+
+CHATBAR_MENU_SHOW_BUTTON			= "Show \"%s\" Button";
+
+CHATBAR_REORDER_START = "Channels are being reordered...";
+CHATBAR_REORDER_END = "Channel reordering finished.";
+CHATBAR_REORDER_FLIGHT_FAIL = "You cannot reorder channels while taxiing.";
+
+-- Capital Cities
+CHATBAR_ORGRIMMAR		= "Orgrimmar";
+CHATBAR_STORMWIND		= "Stormwind City";
+CHATBAR_IRONFORGE		= "Ironforge";
+CHATBAR_DARNASSUS		= "Darnassus";
+CHATBAR_UNDERCITY		= "Undercity";
+CHATBAR_THUNDERBLUFF	= "Thunder Bluff";
+
+CHATBAR_GENERAL				= "General";
+CHATBAR_TRADE				= "Trade";
+CHATBAR_LFG					= "LookingForGroup";
+CHATBAR_LOCALDEFENSE		= "LocalDefense";
+CHATBAR_WORLDDEFENSE		= "WorldDefense";
+CHATBAR_GUILDRECRUITMENT	= "GuildRecruitment";
